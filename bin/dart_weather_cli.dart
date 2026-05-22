4fc0122af50e68387381341c56832bd6058bb918
gs <gs@xyz.de>
2026-05-22 17:21:17 +0200

    first version of ForecastProvider

--- bin/dart_weather_cli.dart
+++ bin/dart_weather_cli.dart
@@ -1,5 +1,18 @@
-import 'package:dart_weather_cli/dart_weather_cli.dart' as dart_weather_cli;
+import 'package:dart_weather_cli/forecast_provider.dart';
+// take care: next line would lead to error "Can't use a relative path to import a library in 'lib'."
+// see https://gemini.google.com/share/f3d605a58226
+//import '../lib/forecast_provider.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_weather_cli.calculate()}!');
+void main(List<String> arguments) async {
+  var forecastProvider = ForecastProvider();
+
+  bool succeeded = await forecastProvider.fetchHourlyForecast();
+  if (!succeeded) {
+    print("fetchHourlyForecast did not succeed.");
+    return;
+  }
+
+  for (var e in forecastProvider.hourlyForecast) {
+    print("${e.localTime}: ${e.temp}°C, ${e.precipAmount}mm, ${e.cloudCover}%");
+  }
 }
