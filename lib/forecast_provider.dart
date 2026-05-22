0e36111e9978b19ee62567af2c3ea435aa3e4eb2
gs <gs@xyz.de>
2026-05-22 17:24:16 +0200

    convert UTC to local time with toimezone package

--- lib/forecast_provider.dart
+++ lib/forecast_provider.dart
@@ -1,20 +1,33 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:lat_lng_to_timezone/lat_lng_to_timezone.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 import 'weather_data.dart';
 
 class ForecastProvider {
   List<WeatherData> hourlyForecast = [];
+  bool _initialized = false;
+
+  void _initialize() {
+    tz.initializeTimeZones();
+  }
 
   Future<bool> fetchHourlyForecast() async {
+    if (!_initialized) {
+      _initialize();
+      _initialized = true;
+    }
+
     hourlyForecast.clear();
 
     // latitude and longitude of Schloß Johannisburg in Aschaffenburg
-    // var lat = 49.97606;
-    // var lon = 9.14163;
+    var lat = 49.97606;
+    var lon = 9.14163;
 
     // latitude and longitude for Sydney
-    var lat = -33.86785;
-    var lon = 151.20732;
+    // var lat = -33.86785;
+    // var lon = 151.20732;
 
     // 'https://api.open-meteo.com/v1/forecast?latitude=49.97606&longitude=9.14163&hourly=temperature_2m,precipitation,cloud_cover&forecast_days=1';
     final url =
@@ -40,8 +53,12 @@ class ForecastProvider {
     precipAmounts = List<double?>.from(data['hourly']['precipitation']);
     cloudCovers = List<int?>.from(data['hourly']['cloud_cover']);
 
+    var timezoneName = latLngToTimezoneString(lat, lon);
+    final tzLocation = tz.getLocation(timezoneName);
+
     for (int i = 0; i < times.length; i++) {
-      final localTime = DateTime.parse(times[i]);
+      final timeInUtc = DateTime.parse(times[i]);
+      final localTime = tz.TZDateTime.from(timeInUtc, tzLocation);
       var data = WeatherData(localTime);
       data.temp = temps[i];
       data.precipAmount = precipAmounts[i];
